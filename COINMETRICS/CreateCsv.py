3db26c205f19650df5ab58a00f0363b374ee01b0
noot1402 <noor.majed@esme.fr>
2022-06-06 22:38:39 +0200

    final changes

--- COINMETRICS/CreateCsv.py
+++ COINMETRICS/CreateCsv.py
@@ -1,10 +1,11 @@
 import pandas as pd
 def get_df(csv):
-    df=pd.read_csv(csv,parse_dates=True)
-    df=df.set_index('time')
+    df=pd.read_csv(csv,parse_dates=True)    
     return df
-def getpartialcsv(csv,date1,date2):
-    df=get_df(csv)
+def getpartialcsv(date1,date2):
+    df=get_df('btc.csv')
+    df=df.set_index('time')
     newdf=df.loc[date1:date2,'PriceUSD']
     newcsv=newdf.to_csv('PartialCSV.csv')
-    return newcsv
+    dffinal=get_df('PartialCSV.csv')
+    return dffinal
